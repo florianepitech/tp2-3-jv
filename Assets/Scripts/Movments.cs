b6f61f3a02642c56d1a43816b9a66f8b55b3d9c7
bastosbobSchool <72014566+bastosbob@users.noreply.github.com>
2023-11-03 15:56:24 -0400

    add: new network and prefab

---
+++ Assets/Scripts/Movments.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class Movement : NetworkBehaviour
+{
+    public float moveSpeed = 5f;
+    private Vector3 currentInput;
+
+    private void Update()
+    {
+        if (IsLocalPlayer)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            currentInput = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+            // Send the updated input to the server
+            SubmitInputToServerRpc(currentInput);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsServer)
+        {
+            HandleMovement(currentInput);
+        }
+    }
+
+    [ServerRpc]
+    void SubmitInputToServerRpc(Vector3 input)
+    {
+        // Server receives client input and applies it
+        currentInput = input;
+    }
+
+    private void HandleMovement(Vector3 input)
+    {
+        // Apply movement on the server
+        Vector3 moveDirection = input * moveSpeed * Time.fixedDeltaTime;
+        transform.position += moveDirection;
+
+        // Update the position on clients
+        RpcUpdatePositionOnClientsClientRpc(transform.position);
+    }
+
+    [ClientRpc]
+    void RpcUpdatePositionOnClientsClientRpc(Vector3 newPosition)
+    {
+        // Update the position on all clients
+        transform.position = newPosition;
+    }
+}

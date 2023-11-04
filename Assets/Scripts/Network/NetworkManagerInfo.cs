4214fe7d6a5723f4440d458a4b4bc7cad375b0cc
bastosbobSchool <72014566+bastosbob@users.noreply.github.com>
2023-11-04 16:38:15 -0400

    fix: delete debug c

---
+++ Assets/Scripts/Network/NetworkManagerInfo.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+using UnityEngine;
+using Unity.Netcode;
+using Unity.Networking.Transport;
+
+public class NetworkManagerController : NetworkBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsServer)
+        {
+         
+        }
+
+    }
+    
+}

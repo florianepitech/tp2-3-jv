bdb65a43823c8d5aea6e34ecaeea6b0c1b010c86
bastosbobSchool <72014566+bastosbob@users.noreply.github.com>
2023-11-04 17:31:49 -0400

    add: clients connected

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,12 +1,15 @@
 using System;
+using System.Collections.Generic;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
 
-public class Game : MonoBehaviour
+public class Game : NetworkBehaviour
 {
     public static GameType gameType;
+    public IReadOnlyList<NetworkClient> connectedClients = new List<NetworkClient>();
 
     // Start is called before the first frame update
     void Start()
@@ -40,6 +43,14 @@ public class Game : MonoBehaviour
         // Check for shoot bar
         spawnShootBar();
     }
+    
+    private void FixedUpdate()
+    {
+        //only the server can get the number of connected clients
+        if (IsServer)
+            getPlayersConnectedServerRpc();
+        
+    }
 
     private void spawnShootBar()
     {
@@ -56,6 +67,14 @@ public class Game : MonoBehaviour
         var pivotPoint = shootBar.transform.position;
         // Rotate the ShootBar around the Y-axis with respect to the pivot point
         shootBar.transform.RotateAround(pivotPoint, Vector3.up, rotation);
+    }
+    
+    
+    [ServerRpc]
+    void getPlayersConnectedServerRpc()
+    {
+         connectedClients = NetworkManager.Singleton.ConnectedClientsList;
+         Debug.Log("Connected clients: " + connectedClients.Count);
     }
     
 }
--- Assets/Scripts/Network/NetworkManagerInfo.cs
+++ Assets/Scripts/Network/NetworkManagerInfo.cs
@@ -15,7 +15,17 @@ public class NetworkManagerController : NetworkBehaviour
     {
         if (IsServer)
         {
-         
+            //print the number of connected clients
+            //list the connected clients
+            var connectedClients = NetworkManager.Singleton.ConnectedClientsList;
+            if (connectedClients.Count > 0)
+            {
+                Debug.Log("Connected clients: " + connectedClients.Count);
+                foreach (var client in connectedClients)
+                {
+                    Debug.Log("Client: " + client.ClientId);
+                }
+            }
         }
 
     }

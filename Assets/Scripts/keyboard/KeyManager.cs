7e2fadd179ad85c717acf9840891ceaca2d97412
Florian <91879250+florianepitech@users.noreply.github.com>
2023-11-15 19:37:58 -0500

    add

--- Assets/Scripts/keyboard/KeyManager.cs
+++ Assets/Scripts/keyboard/KeyManager.cs
@@ -1,35 +1,85 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class KeyManager: MonoBehaviour
+namespace keyboard
 {
-    public string keyName = "";
-    
-    void Update()
+    public class KeyManager : MonoBehaviour
     {
-        var lastKeyPressed = GetLastKeyPressed();
-        if (lastKeyPressed != KeyCode.None)
-        {
-            SaveKey(keyName, lastKeyPressed.ToString());
-        }
-    }
+        public string keyName;
+        
+        public KeyCode defaultValue;
     
-    private static KeyCode GetLastKeyPressed()
-    {
-        var lastKeyPressed = KeyCode.None;
-        foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
+        private TMP_InputField _inputField;
+
+        void Start()
         {
-            if (Input.GetKeyDown(keyCode))
+            _inputField = GetComponent<TMP_InputField>();
+            if (_inputField == null)
             {
-                lastKeyPressed = keyCode;
+                Debug.LogError("TMP input field not found");
+            }
+            var savedKey = GetSavedKey(keyName);
+            if (savedKey != KeyCode.None)
+            {
+                _inputField.text = savedKey.ToString();
+            }
+            else if (defaultValue != KeyCode.None)
+            {
+                _inputField.text = defaultValue.ToString();
             }
         }
+    
+        void Update()
+        {
+            if (!_inputField.isFocused)
+            {
+                return;
+            }
+            var lastKeyPressed = GetLastKeyPressed();
+            if (lastKeyPressed != KeyCode.None)
+            {
+                SaveKey(keyName, lastKeyPressed);
+                _inputField.text = lastKeyPressed.ToString();
+            }
+        }
+    
+        private static KeyCode GetLastKeyPressed()
+        {
+            var lastKeyPressed = KeyCode.None;
+            foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(keyCode))
+                {
+                    lastKeyPressed = keyCode;
+                }
+            }
 
-        return lastKeyPressed;
-    }
+            return lastKeyPressed;
+        }
     
-    private static void SaveKey(string key, string value)
-    {
-        PlayerPrefs.SetString(key, value);
-    }
+        private static void SaveKey(string key, KeyCode value)
+        {
+            if (key == null)
+            {
+                Debug.LogError("Key is null");
+                return;
+            }
+            PlayerPrefs.SetString(key, value.ToString());
+        }
     
+        private static KeyCode GetSavedKey(string key)
+        {
+            if (key == null)
+            {
+                Debug.LogError("Key is null");
+                return KeyCode.None;
+            }
+            var code = PlayerPrefs.GetString(key);
+            return code == "" ?
+                KeyCode.None : (KeyCode) System.Enum.Parse(typeof(KeyCode), code);
+        }
+        
+    }
 }

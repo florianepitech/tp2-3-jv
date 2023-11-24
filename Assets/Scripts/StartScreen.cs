81f60483d26ffdf8dbba7279bbc347d120ab0866
Florian <91879250+florianepitech@users.noreply.github.com>
2023-11-24 18:23:19 -0500

    add: start screen

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If enter is pressed load HomeMenu scene
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("HomeMenu");
+        }
+    }
+}

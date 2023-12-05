d6e08be343bd20ff3b9bf69490d95dd75a5aed98
bastosbobSchool <72014566+bastosbob@users.noreply.github.com>
2023-12-05 18:15:11 -0500

    add: ?

---
+++ Assets/Scripts/Avatar/AvatarData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarData : ScriptableObject
+{
+    public string avatarName;
+    public GameObject hat;
+    public Material primaryColor;
+    public Material secondaryColor;
+}gi
---
+++ Assets/Scripts/Avatar/AvatarManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarManager : MonoBehaviour
+{
+    public List<AvatarData> avatars = new List<AvatarData>();
+}
